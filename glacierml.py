3546b5f46a7ac08e570a018b2a2a0733059e7023
Simon Hans Edasi <woses21@gmail.com>
2022-03-22 13:01:07 -0400

    updated glacier.py workflow

--- glacierml.py
+++ glacierml.py
@@ -19,26 +19,6 @@ def build_linear_model(normalizer):
     
     return model
 
-def plot_single_model_variable(x, y,feature_name):
-    plt.scatter(train_features[feature_name], train_labels, label='Data')
-    plt.plot(x, y, color='k', label='Predictions')
-    plt.xlabel(feature_name)
-    plt.ylabel('Avg Thickness (m)')
-#     plt.xlim((0,20))
-    plt.legend()
-      
-def plot_loss(history):
-#     plt.subplots(figsize=(10,5))
-    plt.plot(history.history['loss'], label='loss')
-    plt.plot(history.history['val_loss'], label='val_loss')
-    #   plt.ylim([0, 10])
-    plt.xlabel('Epoch')
-    plt.ylabel('Error')
-    plt.legend()
-    plt.grid(True)
-    
-    return plot_loss
-
 def build_dnn_model(norm):
     model = keras.Sequential([
               norm,
@@ -51,7 +31,27 @@ def build_dnn_model(norm):
     
     return model
 
-def dnn_test_results(feature_name):
-    dnn_test_results[feature_name] = dnn_model.evaluate(
-        test_features[feature_name],
-        test_labels, verbose=0)
+def plot_loss(history):
+#     plt.subplots(figsize=(10,5))
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    #   plt.ylim([0, 10])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.grid(True)
+    
+    return plot_loss
+
+
+def plot_single_model_variable(x, y,feature_name):
+    plt.scatter(train_features[feature_name], train_labels, label='Data')
+    plt.plot(x, y, color='k', label='Predictions')
+    plt.xlabel(feature_name)
+    plt.ylabel('Avg Thickness (m)')
+#     plt.xlim((0,20))
+    plt.legend()
+      
+
+
+
