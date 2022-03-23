7128bc8df87c69142c384cd9b7f759ce7829699c
Simon Hans Edasi <woses21@gmail.com>
2022-03-23 11:37:45 -0400

    removed glacierml dependancy

--- glacierml.py
+++ glacierml.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
