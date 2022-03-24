6065bc1f3ad5acacbb75fe9b2ba5be7c097bb314
Simon Hans Edasi <woses21@gmail.com>
2022-03-24 19:13:45 -0400

    moved files

--- glacierml.py
+++ glacierml.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 import matplotlib.pyplot as plt
-
+    
 def build_linear_model(normalizer):
     model = tf.keras.Sequential([
         normalizer,
@@ -18,18 +18,14 @@ def build_linear_model(normalizer):
     
     return model
 
-def build_dnn_model(norm):
-    model = keras.Sequential([
-              norm,
-              layers.Dense(64, activation='relu'),
-              layers.Dense(64, activation='relu'),
-              layers.Dense(1) ])
-
-    model.compile(loss='mean_absolute_error',
-                optimizer=tf.keras.optimizers.Adam(0.01))
-    
-    return model
-
+def plot_single_model_variable(x, y,feature_name):
+    plt.scatter(train_features[feature_name], train_labels, label='Data')
+    plt.plot(x, y, color='k', label='Predictions')
+    plt.xlabel(feature_name)
+    plt.ylabel('Avg Thickness (m)')
+#     plt.xlim((0,20))
+    plt.legend()
+      
 def plot_loss(history):
 #     plt.subplots(figsize=(10,5))
     plt.plot(history.history['loss'], label='loss')
@@ -42,15 +38,14 @@ def plot_loss(history):
     
     return plot_loss
 
+def build_dnn_model(norm):
+    model = keras.Sequential([
+              norm,
+              layers.Dense(64, activation='relu'),
+              layers.Dense(64, activation='relu'),
+              layers.Dense(1) ])
 
-def plot_single_model_variable(x, y,feature_name):
-    plt.scatter(train_features[feature_name], train_labels, label='Data')
-    plt.plot(x, y, color='k', label='Predictions')
-    plt.xlabel(feature_name)
-    plt.ylabel('Avg Thickness (m)')
-#     plt.xlim((0,20))
-    plt.legend()
-      
-
-
-
+    model.compile(loss='mean_absolute_error',
+                optimizer=tf.keras.optimizers.Adam(0.01))
+    
+    return model
