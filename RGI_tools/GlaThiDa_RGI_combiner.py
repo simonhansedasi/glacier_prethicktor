434e2b90127d9c076c6210f0302cc86eb667b6fe
Simon Hans Edasi <woses21@gmail.com>
2022-06-14 10:58:39 -0400

    updated data file builder and combiner

--- RGI_tools/GlaThiDa_RGI_combiner.py
+++ RGI_tools/GlaThiDa_RGI_combiner.py
@@ -9,8 +9,8 @@ import chardet
 from chardet import detect
 from tqdm import tqdm
 pth = '/data/fast1/glacierml/T_models/'
-T = pd.read_csv(pth + 'T.csv', low_memory = False)
-T = T.dropna(subset = ['MEAN_THICKNESS'])
+T = pd.read_csv(pth + 'glacier.csv', low_memory = False)
+T = T.dropna(subset = ['mean_thickness'])
 
 rootdir = '/data/fast0/datasets/rgi60-attribs/'
 RGI_extra = pd.DataFrame()
@@ -23,30 +23,41 @@ RGI_coordinates = RGI_extra[[
     'CenLat',
     'CenLon'
 ]]
-RGI_coordinates
 
-L = pd.DataFrame()
+L = pd.DataFrame(columns = ['GlaThiDa_index', 'RGI_index'])
 glac = pd.DataFrame()
 for T_idx in tqdm(T.index):
-    GlaThiDa_coords = (T['LAT'].loc[T_idx],
-                       T['LON'].loc[T_idx])
+    GlaThiDa_coords = (T['lat'].loc[T_idx],
+                       T['lon'].loc[T_idx])
 #     print(GlaThiDa_coords)
     for RGI_idx in RGI_coordinates.index:
 #         print(RGI_idx)
         RGI_coords = (RGI_coordinates['CenLat'].loc[RGI_idx],
                       RGI_coordinates['CenLon'].loc[RGI_idx])
+        
         distance = geopy.distance.geodesic(GlaThiDa_coords,RGI_coords).km
-        if distance == 0:
-#             print('DING!')
-#             print(T_idx)
-#             print(RGI_idx)
+        if 0 <= distance < 1:
 #             print(RGI_coords)
             f = pd.Series(distance, name='distance')
+            L = L.copy()
             L = L.append(f, ignore_index=True)
-            L['GlaThiDa_index'] = T_idx
-            L['RGI_index'] = RGI_idx
-            glac = glac.append(L, ignore_index=True)
-#             glac_combined.to_csv('glac_combined.csv')
-            break
+            L['GlaThiDa_index'].iloc[-1] = T_idx
+            L['RGI_index'].iloc[-1] = RGI_idx
+#             L['distance'] = distance
+            L.to_csv('l.csv')
+#         elif 0 < distance < 1:
+#             f = pd.Series(distance, name='distance')
+#             L = L.copy().append(f, ignore_index=True)
+#             L['GlaThiDa_index'].iloc[-1] = T_idx
+#             L['RGI_index'].iloc[-1] = RGI_idx
+#             L.to_csv('l.csv')
+        
+
+#             print('bing! go fuck yourself')
             
-glac.to_csv('GlaThiDa&RGI_matched_indexes_attempt2.csv')
+#             glac = glac.append(L, ignore_index=True)
+#             glac.drop_duplicates(subset = ['GlaThiDa_index'], keep = 'first')
+#             glac.to_csv('glac_combined.csv')
+            
+            
+# glac.to_csv('GlaThiDa_RGI_matched_indexes.csv')
---
+++ RGI_tools/Glam_2_builder.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import os
+import geopy.distance
+import chardet
+from chardet import detect
+from tqdm import tqdm
+
+comb = pd.read_csv('RGI_tools/l.csv')
+comb = comb.rename(columns = {'0':'distance'})
+
+glacier = pd.read_csv('/data/fast1/glacierml/T_models/glacier.csv')
+glacier = glacier.dropna(subset = ['mean_thickness'])
+
+comb = comb[[
+    'GlaThiDa_index',
+    'RGI_index',
+    'distance'
+]]
+
+combined_indexes = pd.DataFrame()
+for GlaThiDa_index in comb['GlaThiDa_index'].index:
+    df = comb[comb['GlaThiDa_index'] == GlaThiDa_index]
+    f = df.loc[df[df['distance'] == df['distance'].min()].index]
+    combined_indexes = combined_indexes.append(f)
+combined_indexes
+
+combined_indexes = combined_indexes.drop_duplicates(subset = ['GlaThiDa_index'])
+combined_indexes = combined_indexes.reset_index()
+combined_indexes = combined_indexes[[
+    'GlaThiDa_index',
+    'RGI_index',
+    'distance'
+]]
+
+data = pd.DataFrame(columns = ['GlaThiDa_index', 'thickness'])
+for GlaThiDa in combined_indexes['GlaThiDa_index'].index:
+    glathida_thickness = glacier['mean_thickness'].iloc[GlaThiDa] 
+    rgi_index = combined_indexes['RGI_index'].loc[GlaThiDa]  
+    rgi = RGI_extra.iloc[[rgi_index]]
+    
+    data = data.append(rgi)
+    data['GlaThiDa_index'].iloc[-1] = combined_indexes['GlaThiDa_index'].loc[GlaThiDa]
+    data['thickness'].iloc[-1] = glathida_thickness
+#     print(combined_indexes['GlaThiDa_index'].iloc[[GlaThiDa]])
+
+data = data.drop_duplicates(subset = ['RGIId'])
+data = data.reset_index()
+data = data[[
+#     'RGIId',
+    'GlaThiDa_index',
+    'thickness',
+    'CenLon',
+    'CenLat',
+    'Area',
+    'Zmin',
+    'Zmed',
+    'Zmax',
+    'Slope',
+    'Aspect',
+    'Lmax'
+]]
+data.to_csv('Glam_2.csv')
