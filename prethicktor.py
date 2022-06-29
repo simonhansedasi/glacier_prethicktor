b056e1a89a1e1bc97ea2a69c65c16e305bb525ba
Simon Hans Edasi <woses21@gmail.com>
2022-06-29 18:01:27 -0400

    integrated new data loader and updated workflow and CLI

--- prethicktor.py
+++ prethicktor.py
@@ -21,15 +21,18 @@ def main():
 #     LR = 0.1, 0.01, 0.001
     RS = range(0,25,1)
     
+
+    
     # select either to train on all available data, or break up training by regions
 
-    print('please select module: sm01, sm02, sm031, sm1, sm2, sm3, sm4, sm5, sm6')
-    module_list = ('sm01', 'sm02', 'sm031', 'sm1', 'sm2', 'sm3', 'sm4', 'sm5', 'sm6')
+    print('please select module: sm1, sm2, sm3, sm4')
+    module_list = ('sm1', 'sm2', 'sm3', 'sm4', 'sm5', 'sm6')
     module = input()
+    
 
-#         comment out these lines to enable speedy mode, and enter 'all' as a module entry        
+
     while module not in module_list:
-        print('please select valid module: sm01, sm02, sm031, sm1, sm2, sm3, sm4, sm5, sm6')
+        print('please select valid module: sm1, sm2, sm3, sm4, sm5, sm6')
         module = input()
     # here we can select between databases
     # sm1 = original GlaThiDa information
@@ -37,150 +40,147 @@ def main():
     # sm3 = sm2 w/o lat and lon
     # sm4 = GlaThiDa matched with RGI using technique 2 defined in glacierml.py
     # res = variable to construct directory to save results
-    if module == 'sm01':
-        df01 = gl.data_loader_01(
-            pth_1 = '/home/prethicktor/data/T_data/'
-        )
-        gl.thickness_renamer(df01)
-        dataset = df01
-        dataset.name = 'df01'
-        res = 'sr01'
-#         print(module)
-#         print(dataset)
-
-    if module == 'sm02':
-        df02 = gl.data_loader_02(
-            pth_1 = '/home/prethicktor/data/T_data/'
-        )
-        gl.thickness_renamer(df02)
-        dataset = df02
-        dataset.name = 'df02'
-        res = 'sr02'
-#         print(module)
-#         print(dataset)
-
-    if module == 'sm031':
-        df031 = gl.data_loader_031(
-            pth_1 = '/home/prethicktor/data/T_data/',
-            pth_2 = '/home/prethicktor/data/RGI/rgi60-attribs/',
-            pth_3 = '/home/prethicktor/data/matched_indexes/'
-        )
-        gl.thickness_renamer(df031)
-        dataset = df031
-        dataset.name = 'df031'
-        res = 'sr031'
-#         print(module)
-#         print(dataset)
-
+    
+    layer_1_input, layer_2_input, lr_input, ep_input = gl.prethicktor_inputs()
+    arch = str(layer_1_input) + '-' + str(layer_2_input)
+    
 
     if module == 'sm1':
-        df1 = gl.data_loader_1(
+        df1 = gl.data_loader(
             pth_1 = '/home/prethicktor/data/T_data/',
             pth_2 = '/home/prethicktor/data/RGI/rgi60-attribs/',
-            pth_3 = '/home/prethicktor/data/matched_indexes/'
+            pth_3 = '//home/prethicktor/data/matched_indexes/',
+            pth_4 = '/home/prethicktor/data/regional_data/training_data/',
+            RGI_input = 'n'
+#                 scale = 'g',
+#                 region_selection = 1,
+#                 area_scrubber = 'off',
+#                 anomaly_input = 5
         )
-        gl.thickness_renamer(df1)
         dataset = df1
         dataset.name = 'df1'
         res = 'sr1'
 #         print(module)
 #         print(dataset)
 
-    elif module == 'sm2':
-        df2 = gl.data_loader_2(
+    if module == 'sm2':
+        df2 = gl.data_loader(
             pth_1 = '/home/prethicktor/data/T_data/',
             pth_2 = '/home/prethicktor/data/RGI/rgi60-attribs/',
-            pth_3 = '/home/prethicktor/data/matched_indexes/'
+            pth_3 = '//home/prethicktor/data/matched_indexes/',
+            pth_4 = '/home/prethicktor/data/regional_data/training_data/',
+            RGI_input = 'y',
+            scale = 'g',
+#                 region_selection = 1,
+            area_scrubber = 'off'
+#                 anomaly_input = 5
         )
-        gl.thickness_renamer(df2)
         dataset = df2
         dataset.name = 'df2'
         res = 'sr2'
-#         print(module)
-#         print(dataset)
 
-    elif module == 'sm3':
-        df3 = gl.data_loader_3(
+    if module == 'sm3':
+        df3 = gl.data_loader(
             pth_1 = '/home/prethicktor/data/T_data/',
             pth_2 = '/home/prethicktor/data/RGI/rgi60-attribs/',
-            pth_3 = '/home/prethicktor/data/matched_indexes/'
+            pth_3 = '//home/prethicktor/data/matched_indexes/',
+            pth_4 = '/home/prethicktor/data/regional_data/training_data/',
+            RGI_input = 'y',
+            scale = 'g',
+#                 region_selection = 1,
+            area_scrubber = 'on',
+            anomaly_input = 1
         )
-        gl.thickness_renamer(df3)
         dataset = df3
         dataset.name = 'df3'
         res = 'sr3'
-#         print(module)
-#         print(dataset)
 
-    elif module == 'sm4':
-        df4 = gl.data_loader_4(
+    if module == 'sm4':
+        df4 = gl.data_loader(
             pth_1 = '/home/prethicktor/data/T_data/',
             pth_2 = '/home/prethicktor/data/RGI/rgi60-attribs/',
-            pth_3 = '/home/prethicktor/data/matched_indexes/'
+            pth_3 = '//home/prethicktor/data/matched_indexes/',
+            pth_4 = '/home/prethicktor/data/regional_data/training_data/',
+            RGI_input = 'y',
+            scale = 'g',
+#                 region_selection = 1,
+            area_scrubber = 'on',
+            anomaly_input = 5
         )
-        gl.thickness_renamer(df4)
         dataset = df4
         dataset.name = 'df4'
         res = 'sr4'
-#         print(module)
-#         print(dataset)
         
-    elif module == 'sm5':
-        df5 = gl.data_loader_5(pth = '/home/prethicktor/data/regional_data/rd1/training_data/')
+    if module == 'sm5':
+        df5 = gl.data_loader(
+            pth_1 = '/home/prethicktor/data/T_data/',
+            pth_2 = '/home/prethicktor/data/RGI/rgi60-attribs/',
+            pth_3 = '//home/prethicktor/data/matched_indexes/',
+            pth_4 = '/home/prethicktor/data/regional_data/training_data/',
+            RGI_input = 'y',
+            scale = 'g',
+            region_selection = 1,
+            area_scrubber = 'on',
+            anomaly_input = 5
+        )
+        dataset = df5
+        dataset.name = 'df5'
         reg = df5['region'].iloc[-1]
         df5 = df5.drop('region', axis=1)
         dataset = df5
         dataset.name = str('df5_' + str(reg))
-        res = 'sr5'
-#         print(module)
-#         print(dataset)            
 
+    #code snippet to add a leading 0 to regional ID so it matches with RGI when built later
+        if len(str(reg)) ==1:
+            N = 1
+            reg = str(reg).zfill(N + len(str(reg)))
+        else:
+            reg = reg
 
-    elif module == 'sm6':
-        df6 = gl.data_loader_6(pth = '/home/prethicktor/data/regional_data/rd2/training_data/')
-        reg = df6['region'].iloc[-1]
-        df6 = df6.drop('region', axis=1)
-        dataset = df6 
-        dataset.name = str('df6_' + str(reg))
-        res = 'sr6'
-#         print(module)
-#         print(dataset)
-
-    layer_1_input, layer_2_input, lr_input, ep_input, dropout = gl.prethicktor_inputs()
-    arch = str(layer_1_input) + '-' + str(layer_2_input)
-    
-    print(
-        'Running multi-variable DNN regression on ' + 
-        str(dataset.name) + 
-        ' dataset with parameters: Learning Rate = ' + 
-        str(lr_input) + 
-        ', Layer Architechture = ' +
-        arch +
-        ', dropout = ' + 
-        str(dropout) +
-        ', Validation split = ' + 
-        str(0.2) + 
-        ', Epochs = ' + 
-        str(ep_input) 
-    )
-    
-    for rs in tqdm(RS):
-#             for lr in LR:
-        
-        gl.build_and_train_model(
-            dataset, 
-            learning_rate = float(lr_input), 
-            random_state = rs, 
-            epochs = int(ep_input), 
-            module = module, 
-            res = res,
-            layer_1 = layer_1_input,
-            layer_2 = layer_2_input,
-            dropout = dropout
+#     layer_1_input, layer_2_input, lr_input, ep_input, dropout = gl.prethicktor_inputs()
+#     arch = str(layer_1_input) + '-' + str(layer_2_input)
+    dropout_input_list = ('y', 'n')
+    for dropout_input_iter in dropout_input_list:
+        dropout_input = dropout_input_iter
+        if dropout_input == 'y':
+            dropout = True
+        elif dropout_input == 'n':
+            dropout = False
+        print(
+            'Running multi-variable DNN regression on ' + 
+            str(dataset.name) + 
+            ' dataset with parameters: Learning Rate = ' + 
+            str(lr_input) + 
+            ', Layer Architechture = ' +
+            arch +
+            ', dropout = ' + 
+            str(dropout) +
+            ', Validation split = ' + 
+            str(0.2) + 
+            ', Epochs = ' + 
+            str(ep_input) 
         )
-    # here we can select between two regional datasets. 
-    # sm5 uses matching technique 1, same to build sm2
-    # sm6 uses matching technique 2, same to build sm4
+
+
+    
+    
+        for rs in tqdm(RS):
+    #             for lr in LR:
+
+            gl.build_and_train_model(
+                dataset, 
+                learning_rate = float(lr_input), 
+                random_state = rs, 
+                epochs = int(ep_input), 
+                module = module, 
+                res = res,
+                layer_1 = layer_1_input,
+                layer_2 = layer_2_input,
+                dropout = dropout
+            )
+# here we can select between two regional datasets. 
+# sm5 uses matching technique 1, same to build sm2
+# sm6 uses matching technique 2, same to build sm4
 
             
 
