c6a8fd8c29d786af80e7873ec3120d27eb4c34f4
Simon Hans Edasi <woses21@gmail.com>
2022-06-29 18:02:19 -0400

    updated loop to load both dropout and non-droput layers

--- results_builder.py
+++ results_builder.py
@@ -18,186 +18,199 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 pd.set_option('mode.chained_assignment', None)
 
 
-    
-print('please select module: sm01, sm02, sm031, sm1, sm2, sm3, sm4, sm5, sm6')
+
+
+
+print('please select module: sm1, sm2, sm3, sm4')
 dir_list = ('sm01', 'sm02', 'sm1', 'sm2', 'sm031', 'sm3', 'sm4', 'sm5', 'sm6', 'all')
 
 chosen_dir = input()
 
 while chosen_dir not in dir_list:
-    print('Please enter valid module selection: sm1, sm2, sm3, sm4','sm5','sm6')
+    print('Please enter valid module selection: sm1, sm2, sm3, sm4')
     chosen_dir = input()    
 
-if chosen_dir == 'sm01':
-    df01 = gl.data_loader_01()
-    gl.thickness_renamer(df01)
-    dataset = df01
-    dataset.name = 'df01'
 
-if chosen_dir == 'sm02':
-    df02 = gl.data_loader_02()
-    gl.thickness_renamer(df02)
-    dataset = df02
-    dataset.name = 'df02'
-    
-if chosen_dir == 'sm031':
-    df031 = gl.data_loader_031()
-    gl.thickness_renamer(df031)
-    dataset = df031
-    dataset.name = 'df031'
-    
-    
-    
+
+
 if chosen_dir == 'sm1':
-    df1 = gl.data_loader_1()
-    gl.thickness_renamer(df1)
+    df1 = gl.data_loader(
+        RGI_input = 'n'
+#                 scale = 'g',
+#                 region_selection = 1,
+#                 area_scrubber = 'off',
+#                 anomaly_input = 5
+    )
     dataset = df1
     dataset.name = 'df1'
+    res = 'sr1'
+#         print(module)
+#         print(dataset)
 
-elif chosen_dir == 'sm2':
-    df2 = gl.data_loader_2()
+if chosen_dir == 'sm2':
+    df2 = gl.data_loader(
+        RGI_input = 'y',
+        scale = 'g',
+#                 region_selection = 1,
+        area_scrubber = 'off'
+#                 anomaly_input = 5
+    )
     dataset = df2
     dataset.name = 'df2'
+    res = 'sr2'
 
-elif chosen_dir == 'sm3':
-    df3 = gl.data_loader_3()
-    gl.thickness_renamer(df3)
+if chosen_dir == 'sm3':
+    df3 = gl.data_loader(
+        RGI_input = 'y',
+        scale = 'g',
+#                 region_selection = 1,
+        area_scrubber = 'on',
+        anomaly_input = 1
+    )
     dataset = df3
     dataset.name = 'df3'
+    res = 'sr3'
 
-elif chosen_dir == 'sm4':
-    df4 = gl.data_loader_4()
-    gl.thickness_renamer(df4)
+if chosen_dir == 'sm4':
+    df4 = gl.data_loader(
+        RGI_input = 'y',
+        scale = 'g',
+#                 region_selection = 1,
+        area_scrubber = 'on',
+        anomaly_input = 5
+    )
     dataset = df4
     dataset.name = 'df4'
-    
-elif chosen_dir == 'sm5':
-    df5 = gl.data_loader_5()
-    reg = df5['region'].iloc[-1]
-    df5 = df5.drop('region', axis=1)
-    dataset = df5
-    dataset.name = str('df5_' + str(reg))
-    
-    #code snippet to add a leading 0 to regional ID so it matches with RGI when built later
-    if len(str(reg)) ==1:
-        N = 1
-        reg = str(reg).zfill(N + len(str(reg)))
-    else:
-        reg = reg
+    res = 'sr4'
 
-elif chosen_dir == 'sm6':
-    df6 = gl.data_loader_6()
-    reg = df6['region'].iloc[-1]
-    df6 = df6.drop('region', axis=1)
-    dataset = df6
-    dataset.name = str('df6_' + str(reg))
-    
-    #code snippet to add a leading 0 to regional ID so it matches with RGI when built later
-    if len(str(reg)) ==1:
-        N = 1
-        reg = str(reg).zfill(N + len(str(reg)))
-    else:
-        reg = reg
+# elif chosen_dir == 'sm5':
+#     df5 = gl.data_loader()
+#     reg = df5['region'].iloc[-1]
+#     df5 = df5.drop('region', axis=1)
+#     dataset = df5
+#     dataset.name = str('df5_' + str(reg))
 
-global_list = ('sm01', 'sm02', 'sm1', 'sm2', 'sm3', 'sm4')
+#     #code snippet to add a leading 0 to regional ID so it matches with RGI when built later
+#     if len(str(reg)) ==1:
+#         N = 1
+#         reg = str(reg).zfill(N + len(str(reg)))
+#     else:
+#         reg = reg
+
+# elif chosen_dir == 'sm6':
+#     df6 = gl.data_loader()
+#     reg = df6['region'].iloc[-1]
+#     df6 = df6.drop('region', axis=1)
+#     dataset = df6
+#     dataset.name = str('df6_' + str(reg))
+
+#     #code snippet to add a leading 0 to regional ID so it matches with RGI when built later
+#     if len(str(reg)) ==1:
+#         N = 1
+#         reg = str(reg).zfill(N + len(str(reg)))
+#     else:
+#         reg = reg
+
+global_list = ('sm1', 'sm2', 'sm3', 'sm4')
 region_list = ('sm5', 'sm6')
-        
-print('Load dropout layers? y / n')
 
-dropout_input = input()
-dropout_input_list = ('y', 'n')
-while dropout_input not in dropout_input_list:
-    print('Please select valid input: y / n')
-    dropout_input = input()
-if dropout_input == 'y':
-    dropout = '1'
-elif dropout_input == 'n':
-    dropout = '0'
-
-(train_features, test_features, train_labels, test_labels) = gl.data_splitter(dataset)
 
 
 
 rootdir = 'saved_models/' + chosen_dir + '/'
-
+(train_features, test_features, train_labels, test_labels) = gl.data_splitter(dataset)
 dnn_model = {}
 predictions = pd.DataFrame()
 print('loading and evaluating models...')
-for arch in os.listdir(rootdir):
-    print('layer architecture: ' + arch[3:])
-    for folder in tqdm(os.listdir(rootdir + arch)):
-        if 'MULTI' in folder and 'dnn' in folder and '_' + dropout + '_' in folder:
-            dnn_model[arch[3:] + '_' + folder] = tf.keras.models.load_model(
-                rootdir + 
-                arch + 
-                '/' + 
-                folder
-            )
-            mae_test = dnn_model[arch[3:] + '_' + folder].evaluate(
-                test_features, test_labels, verbose=0
-            )
-            mae_train = dnn_model[arch[3:] + '_' + folder].evaluate(
-                train_features, train_labels, verbose=0
-            )
+dropout_input_list = ('y', 'n')
+for dropout_input_iter in dropout_input_list:
+    dropout_input = dropout_input_iter
+    if dropout_input == 'y':
+        dropout = '1'
+    elif dropout_input == 'n':
+        dropout = '0'
+    for arch in os.listdir(rootdir):
+        if dropout == '1':
+            print('layer architecture: ' + arch[3:] + ' dropout = True')
+        elif dropout == '0':
+            print('layer architecture: ' + arch[3:] + ' dropout = False')
+        for folder in tqdm(os.listdir(rootdir + arch)):
+            if '_' + dropout + '_' in folder:
+                dnn_model[arch[3:] + '_' + folder] = tf.keras.models.load_model(
+                    rootdir + 
+                    arch + 
+                    '/' + 
+                    folder
+                )
+                mae_test = dnn_model[arch[3:] + '_' + folder].evaluate(
+                    test_features, test_labels, verbose=0
+                )
+                mae_train = dnn_model[arch[3:] + '_' + folder].evaluate(
+                    train_features, train_labels, verbose=0
+                )
 
-            pred_train = dnn_model[arch[3:] + '_' + folder].predict(
-                train_features, verbose=0
-            )
+                pred_train = dnn_model[arch[3:] + '_' + folder].predict(
+                    train_features, verbose=0
+                )
 
-            pred_test = dnn_model[arch[3:] + '_' + folder].predict(
-                test_features, verbose=0
-            )
+                pred_test = dnn_model[arch[3:] + '_' + folder].predict(
+                    test_features, verbose=0
+                )
 
-            avg_thickness = pd.Series(
-                np.mean(pred_train), name = 'avg train thickness'
-            )
+                avg_thickness = pd.Series(
+                    np.mean(pred_train), name = 'avg train thickness'
+                )
 
-            avg_test_thickness = pd.Series(
-                np.mean(pred_test),  name = 'avg test thickness'
-            )
+                avg_test_thickness = pd.Series(
+                    np.mean(pred_test),  name = 'avg test thickness'
+                )
 
-            temp_df = pd.merge(
-                avg_thickness, avg_test_thickness, right_index=True, left_index=True
-            )
+                temp_df = pd.merge(
+                    avg_thickness, avg_test_thickness, right_index=True, left_index=True
+                )
 
-            predictions = predictions.append(temp_df, ignore_index = True)
-            predictions.loc[predictions.index[-1], 'model'] = folder
-            predictions.loc[predictions.index[-1], 'test mae'] = mae_test
-            predictions.loc[predictions.index[-1], 'train mae'] = mae_train
-            predictions.loc[predictions.index[-1], 'architecture'] = arch[3:]
-            predictions.loc[predictions.index[-1], 'validation split'] = '0.2'
-            predictions.loc[predictions.index[-1], 'dataset'] = dataset.name
-            predictions.loc[predictions.index[-1], 'dropout'] = dropout
-            
-            if chosen_dir in global_list:
-                predictions.loc[predictions.index[-1], 'region'] = 'g'
-            if chosen_dir in region_list:
-                predictions.loc[predictions.index[-1], 'region'] = int(reg)
+                predictions = predictions.append(temp_df, ignore_index = True)
+                predictions.loc[predictions.index[-1], 'model'] = folder
+                predictions.loc[predictions.index[-1], 'test mae'] = mae_test
+                predictions.loc[predictions.index[-1], 'train mae'] = mae_train
+                predictions.loc[predictions.index[-1], 'architecture'] = arch[3:]
+                predictions.loc[predictions.index[-1], 'validation split'] = '0.2'
+                predictions.loc[predictions.index[-1], 'dataset'] = dataset.name
+                predictions.loc[predictions.index[-1], 'dropout'] = dropout
 
-            if '0.1' in folder:
-                predictions.loc[predictions.index[-1], 'learning rate'] = '0.1'
-            if '0.01' in folder:
-                predictions.loc[predictions.index[-1], 'learning rate'] = '0.01'
-            if '0.001' in folder:
-                predictions.loc[predictions.index[-1], 'learning rate']= '0.001'
-            if '25' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '25'
-            if '35' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '35'
-            if '100' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '100'
-            if '150' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '150'
-            if '200' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '200'       
+    #                 if chosen_dir in global_list:
+    #                     predictions.loc[predictions.index[-1], 'region'] = 'g'
+    #                 if chosen_dir in region_list:
+    #                     predictions.loc[predictions.index[-1], 'region'] = int(reg)
 
-            if '300' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '300'
-            if '400' in folder:
-                predictions.loc[predictions.index[-1], 'epochs']= '400'
-                
-predictions.rename(columns = {0:'avg train thickness'},inplace = True)
-predictions.to_csv('zults/predictions_' + dataset.name + '_' + dropout + '.csv')
+                if '0.1' in folder:
+                    predictions.loc[predictions.index[-1], 'learning rate'] = '0.1'
+                if '0.01' in folder:
+                    predictions.loc[predictions.index[-1], 'learning rate'] = '0.01'
+                if '0.001' in folder:
+                    predictions.loc[predictions.index[-1], 'learning rate']= '0.001'
+                if '45' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '45'
+                if '25' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '25'
+                if '35' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '35'
+                if '50' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '50'
+                if '100' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '100'
+                if '150' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '150'
+                if '200' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '200'       
+
+                if '300' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '300'
+                if '400' in folder:
+                    predictions.loc[predictions.index[-1], 'epochs']= '400'
+        
+    predictions.rename(columns = {0:'avg train thickness'},inplace = True)
+    predictions.to_csv('zults/predictions_' + dataset.name + '_' + dropout + '.csv')
 
 # calculate statistics
 print('calculating statistics...')
@@ -259,7 +272,7 @@ for architecture in list(predictions['architecture'].unique()):
                             str(int(epochs)) +
                             '_0'
                         ].count_params() 
-                    
+
                     deviations.loc[
                         deviations.index[-1], 'total inputs'
                     ] = (len(dataset) * (len(dataset.columns) -1))
@@ -307,8 +320,8 @@ for architecture in list(predictions['architecture'].unique()):
                         deviations.index[-1], 'train predicted thickness std dev'
                     ] = train_thickness_std_dev
 
-                     
-                    
+
+
                     deviations.drop(columns = {0},inplace = True)    
                     deviations = deviations.dropna()
 
@@ -318,29 +331,6 @@ for architecture in list(predictions['architecture'].unique()):
                     deviations.to_csv('zults/deviations_' + dataset.name + '_' + dropout + '.csv')
 
 
-
-
-
-# rootdir = 'zults/'
-# predictions = pd.DataFrame()
-# deviations = pd.DataFrame()
-# for file in tqdm(os.listdir(rootdir)):
-#     if 'predictions' in file:
-#         file_reader = pd.read_csv(rootdir + file)
-#         predictions = predictions.append(file_reader, ignore_index = True)
-    
-#     if 'deviations' in file:
-#         file_reader = pd.read_csv(rootdir + file)
-#         deviations = deviations.append(file_reader, ignore_index = True)
-        
-# dataset = deviations['df'].loc[0]
-(train_features, test_features, train_labels, test_labels) = gl.data_splitter(dataset)
-
-# load and evaluate models
-
-#build RGI specific to modules chosen
-
-
 print('loading RGI...')
 rootdir = '/data/fast0/datasets/rgi60-attribs/'
 RGI_extra = pd.DataFrame()
@@ -348,8 +338,8 @@ for file in os.listdir(rootdir):
     file_reader = pd.read_csv(rootdir+file, encoding_errors = 'replace', on_bad_lines = 'skip')
     RGI_extra = RGI_extra.append(file_reader, ignore_index = True)
 
-# select only RGI data that was used to train the model   
-RGI = RGI_extra[[
+    # select only RGI data that was used to train the model   
+    RGI = RGI_extra[[
     'CenLat',
     'CenLon',
     'Slope',
@@ -359,7 +349,7 @@ RGI = RGI_extra[[
     'Area',
     'Aspect',
     'Lmax'
-]]
+    ]]
 
 RGI = RGI.drop(RGI.loc[RGI['Zmed']<0].index)
 RGI = RGI.drop(RGI.loc[RGI['Lmax']<0].index)
@@ -376,45 +366,20 @@ RGI = RGI.drop('index', axis=1)
 
 
 
-if chosen_dir == 'sm01':
+if chosen_dir == 'sm1':
     RGI = RGI.rename(columns = {
-        'CenLat':'lat',
-        'CenLon':'lon',
-        'Area':'area',
-        'Slope':'mean_slope'
+        'CenLat':'Lat',
+        'CenLon':'Lon',
+        'Area':'Area',
+        'Slope':'Mean Slope'
     })
     RGI = RGI[[
-        'lat',
-        'lon',
-        'area',
-        'mean_slope'
-    ]]
-    
-if chosen_dir == 'sm02':
-    RGI = RGI.rename(columns = {
-        'CenLat':'LAT',
-        'CenLon':'LON',
-        'Area':'AREA',
-        'Slope':'MEAN_SLOPE'
-    })
-    RGI = RGI[[
-        'LAT',
-        'LON',
-        'AREA',
-        'MEAN_SLOPE'
-    ]]
-    
-    
-    
-if chosen_dir == 'sm031':
-    RGI = RGI[[
-        'CenLat',
-        'CenLon',
-        'Lmax',
-        'Zmed',
+        'Lat',
+        'Lon',
         'Area',
-        'Slope'
+        'Mean Slope'
     ]]
+
 
 
 
@@ -456,20 +421,20 @@ if chosen_dir == 'sm5' or chosen_dir == 'sm6':
 
 
 deviations = deviations [[
-    'layer architecture',
-    'model parameters',
-    'total inputs',
-    'learning rate',
-    'epochs',
-    'test mae avg',
-    'train mae avg',
-    'test mae std dev',
-    'train mae std dev'
+'layer architecture',
+'model parameters',
+'total inputs',
+'learning rate',
+'epochs',
+'test mae avg',
+'train mae avg',
+'test mae std dev',
+'train mae std dev'
 ]]
-    
-    
-    
-    
+
+
+
+
 print(deviations.to_string())
 # here we can select an entry from the deviations table to make predictions. Default is top entry
 print('Please select model index to predict thicknesses for RGI')
@@ -489,8 +454,8 @@ print('predicting RGI thicknesses using model trained on RGI data matched with G
 RS = range(0,25,1)
 dfs = pd.DataFrame()
 for rs in tqdm(RS):
-    # each series is one random state of an ensemble of 25.
-    # predictions are made on each random state and appended to a df as a column
+# each series is one random state of an ensemble of 25.
+# predictions are made on each random state and appended to a df as a column
     s = pd.Series(
         dnn_model[
             str(arch) +
@@ -535,7 +500,17 @@ for i in tqdm(dfs.index):
     RGI_prethicked['predicted thickness std dev'].loc[i] = predicted_thickness_std_dev
 
 RGI_prethicked.to_csv(
-    'zults/RGI_predicted_' + dataset.name + '_' + dropout + '_' + arch + '_' + str(lr) + '_' + str(ep) + '.csv'
+'zults/RGI_predicted_' 
++ dataset.name + 
+'_' + 
+dropout + 
+'_' + 
+arch + 
+'_' + 
+str(lr) + 
+'_' + 
+str(ep) + 
+'.csv'
 )
 
 
@@ -543,6 +518,6 @@ RGI_prethicked.to_csv(
 
 
 
-        
-        
+
+
 
