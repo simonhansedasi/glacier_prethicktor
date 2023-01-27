16a5e0ee6157541aeb0c7261e3375f5f624c3965
Simon Hans Edasi <woses21@gmail.com>
2023-01-26 21:57:20 -0800

    fixed function call error

--- glacierml.py
+++ glacierml.py
@@ -88,7 +88,7 @@ def parameterize_data(parameterization = '1'):
     config = configparser.ConfigParser()
     config.read('model_parameterization.txt')
 
-    data = gl.load_training_data(
+    data = load_training_data(
     #         root_dir = '/home/prethicktor/data/',
         area_scrubber = config[parameterization]['area scrubber'],
         anomaly_input = float(config[parameterization]['size threshold'])
@@ -703,8 +703,8 @@ def assess_model_performance(parameterization = '1'):
             )
 
             model_name = folder
-            dnn_model = gl.load_dnn_model(model_loc)
-            df = gl.evaluate_model(architecture, model_name, data, dnn_model, parameterization)
+            dnn_model = load_dnn_model(model_loc)
+            df = evaluate_model(architecture, model_name, data, dnn_model, parameterization)
 
             model_predictions = pd.concat([model_predictions, df], ignore_index = True)
     model_predictions.rename(columns = {0:'avg train thickness'},inplace = True)
@@ -731,8 +731,8 @@ def assess_model_performance(parameterization = '1'):
         elif isdir == True:
 
 
-            dnn_model = gl.load_dnn_model(model_loc)
-            df = gl.calculate_model_avg_statistics(
+            dnn_model = load_dnn_model(model_loc)
+            df = calculate_model_avg_statistics(
                 dnn_model,
                 arch,
                 data,
@@ -961,15 +961,15 @@ def calculate_RGI_thickness_statistics(model_statistics, parameterization, useMP
     # aggregate model thicknesses
 #     print('Gathering architectures...')
     arch_list = model_statistics.sort_values('layer architecture')
-#     arch_list = gl.list_architectures(parameterization = parameterization)
+#     arch_list = list_architectures(parameterization = parameterization)
 #     arch_list = arch_list.sort_values('layer architecture')
 #     arch_list = arch_list.reset_index()
 #     arch_list = arch_list.drop('index', axis = 1)
 
-    aggregate_statistics(arch_list)
+    aggregate_statistics(arch_list, parameterization)
 
 
-def aggregate_statistics(arch):
+def aggregate_statistics(arch_list, parameterization):
     df = pd.DataFrame(columns = {
             'RGIId','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
             '11','12','13','14','15','16','17','18','19','20','21',
@@ -978,100 +978,100 @@ def aggregate_statistics(arch):
 #     print('Architectures listed')
     print('Compiling predictions...')
     for arch in tqdm(arch_list['layer architecture'].unique()):
-        df_glob = gl.load_global_predictions(
+        df_glob = load_global_predictions(
             parameterization = parameterization,
             architecture = arch
         )
 
         df = pd.concat([df,df_glob])
 
-        statistics = pd.DataFrame()
-        for file in (os.listdir('zults/')):
-            if 'statistics_' + parameterization in file:
-                file_reader = pd.read_csv('zults/' + file)
-                statistics = pd.concat([statistics, file_reader], ignore_index = True)
+    statistics = pd.DataFrame()
+    for file in (os.listdir('zults/')):
+        if 'statistics_' + parameterization in file:
+            file_reader = pd.read_csv('zults/' + file)
+            statistics = pd.concat([statistics, file_reader], ignore_index = True)
 
-        df = pd.merge(df, statistics, on = 'layer architecture')
-        df = df[[
-                'RGIId','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
-                '11','12','13','14','15','16','17','18','19','20','21',
-                '22','23','24','architecture weight 1'
-        ]]
-        compiled_raw = df.groupby('RGIId')[
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
-                '11','12','13','14','15','16','17','18','19','20','21',
-                '22','23','24','architecture weight 1'
-        ]
+    df = pd.merge(df, statistics, on = 'layer architecture')
+    df = df[[
+            'RGIId','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
+            '11','12','13','14','15','16','17','18','19','20','21',
+            '22','23','24','architecture weight 1'
+    ]]
+    compiled_raw = df.groupby('RGIId')[
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
+            '11','12','13','14','15','16','17','18','19','20','21',
+            '22','23','24','architecture weight 1'
+    ]
 
-        print('Predictions compiled')
-        print('Aggregating statistics...')
-        dft = pd.DataFrame()
-        for this_rgi_id, obj in tqdm(compiled_raw):
-            rgi_id = pd.Series(this_rgi_id, name = 'RGIId')
-    #         print(f"Data associated with RGI_ID = {this_rgi_id}:")
-            dft = pd.concat([dft, rgi_id])
-            dft = dft.reset_index()
-            dft = dft.drop('index', axis = 1)
-
-
-            obj['weight'] = obj['architecture weight 1'] + 1 / (obj[
-                ['0', '1', '2', '3', '4',
-                 '5', '6', '7', '8', '9',
-                 '10','11','12','13','14',
-                 '15','16','17','18','19',
-                 '20','21','22','23','24']
-            ].var(axis = 1))
+    print('Predictions compiled')
+    print('Aggregating statistics...')
+    dft = pd.DataFrame()
+    for this_rgi_id, obj in tqdm(compiled_raw):
+        rgi_id = pd.Series(this_rgi_id, name = 'RGIId')
+#         print(f"Data associated with RGI_ID = {this_rgi_id}:")
+        dft = pd.concat([dft, rgi_id])
+        dft = dft.reset_index()
+        dft = dft.drop('index', axis = 1)
 
 
-            obj['weighted mean'] = obj['weight'] * obj[
-                ['0', '1', '2', '3', '4',
-                 '5', '6', '7', '8', '9',
-                 '10','11','12','13','14',
-                 '15','16','17','18','19',
-                 '20','21','22','23','24']
-            ].mean(axis = 1)
+        obj['weight'] = obj['architecture weight 1'] + 1 / (obj[
+            ['0', '1', '2', '3', '4',
+             '5', '6', '7', '8', '9',
+             '10','11','12','13','14',
+             '15','16','17','18','19',
+             '20','21','22','23','24']
+        ].var(axis = 1))
 
 
-            weighted_glacier_mean = sum(obj['weighted mean']) / sum(obj['weight'])
+        obj['weighted mean'] = obj['weight'] * obj[
+            ['0', '1', '2', '3', '4',
+             '5', '6', '7', '8', '9',
+             '10','11','12','13','14',
+             '15','16','17','18','19',
+             '20','21','22','23','24']
+        ].mean(axis = 1)
 
 
-            stacked_object = obj[[
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
-                '11','12','13','14','15','16','17','18','19','20','21',
-                '22','23','24',
-            ]].stack()
-
-            glacier_count = len(stacked_object)
-        #     dft.loc[dft.index[-1], 'Weighted Mean Thickness'] = weighted_glacier_mean
-            dft.loc[dft.index[-1], 'Mean Thickness'] = stacked_object.mean()
-            dft.loc[dft.index[-1], 'Median Thickness'] = stacked_object.median()
-            dft.loc[dft.index[-1],'Thickness Std Dev'] = stacked_object.std()
-
-            statistic, p_value = shapiro(stacked_object)    
-            dft.loc[dft.index[-1],'Shapiro-Wilk statistic'] = statistic
-            dft.loc[dft.index[-1],'Shapiro-Wilk p_value'] = p_value
+        weighted_glacier_mean = sum(obj['weighted mean']) / sum(obj['weight'])
 
 
-            q75, q25 = np.percentile(stacked_object, [75, 25])    
-            dft.loc[dft.index[-1],'IQR'] = q75 - q25 
+        stacked_object = obj[[
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10',
+            '11','12','13','14','15','16','17','18','19','20','21',
+            '22','23','24',
+        ]].stack()
 
-            lower_bound = np.percentile(stacked_object, 50 - 34.1)
-            median = np.percentile(stacked_object, 50)
-            upper_bound = np.percentile(stacked_object, 50 + 34.1)
+        glacier_count = len(stacked_object)
+    #     dft.loc[dft.index[-1], 'Weighted Mean Thickness'] = weighted_glacier_mean
+        dft.loc[dft.index[-1], 'Mean Thickness'] = stacked_object.mean()
+        dft.loc[dft.index[-1], 'Median Thickness'] = stacked_object.median()
+        dft.loc[dft.index[-1],'Thickness Std Dev'] = stacked_object.std()
 
-            dft.loc[dft.index[-1],'Lower Bound'] = lower_bound
-            dft.loc[dft.index[-1],'Upper Bound'] = upper_bound
-            dft.loc[dft.index[-1],'Median Value'] = median
-            dft.loc[dft.index[-1],'Total estimates'] = glacier_count
+        statistic, p_value = shapiro(stacked_object)    
+        dft.loc[dft.index[-1],'Shapiro-Wilk statistic'] = statistic
+        dft.loc[dft.index[-1],'Shapiro-Wilk p_value'] = p_value
 
-        dft = dft.rename(columns = {
-            0:'RGIId'
-        })
-        dft = dft.drop_duplicates()
-        dft.to_csv(
-            'predicted_thicknesses/sermeq_aggregated_bootstrap_predictions_parameterization_' + 
-            parameterization + '.csv'
-                  )
+
+        q75, q25 = np.percentile(stacked_object, [75, 25])    
+        dft.loc[dft.index[-1],'IQR'] = q75 - q25 
+
+        lower_bound = np.percentile(stacked_object, 50 - 34.1)
+        median = np.percentile(stacked_object, 50)
+        upper_bound = np.percentile(stacked_object, 50 + 34.1)
+
+        dft.loc[dft.index[-1],'Lower Bound'] = lower_bound
+        dft.loc[dft.index[-1],'Upper Bound'] = upper_bound
+        dft.loc[dft.index[-1],'Median Value'] = median
+        dft.loc[dft.index[-1],'Total estimates'] = glacier_count
+
+    dft = dft.rename(columns = {
+        0:'RGIId'
+    })
+    dft = dft.drop_duplicates()
+    dft.to_csv(
+        'predicted_thicknesses/sermeq_aggregated_bootstrap_predictions_parameterization_' + 
+        parameterization + '.csv'
+              )
 
 
 '''
